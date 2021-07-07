afc1a1ecfee7c3674eab04f86709b03775ce8b7a
Jennifer Ong <jennifer.ex.ong@gmail.com>
2021-07-07 23:56:56 +1200

    attach camera to train; camera control with mouse movement

--- src/main.js
+++ src/main.js
@@ -1,83 +1,80 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
 
-class Scene {
-  scene;
-  camera;
-  renderer;
-  controls;
-  mixer;
-  train;
-  clock;
-
-  // setup
-  constructor() {
-    this.clock = new THREE.Clock();
-    this.scene = new THREE.Scene();
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    this.camera.position.z = 5;
-    this.camera.position.y = 2;
-    this.renderer = new THREE.WebGLRenderer({
-      antialias: true,
-    });
-    this.renderer.setSize(width, height);
-    document.body.appendChild(this.renderer.domElement);
-
-    // set background
-    this.scene.background = new THREE.Color("skyblue");
-
-    // sun (natural light)
-    const ambient = new THREE.AmbientLight("gray");
-    const light = new THREE.PointLight("white", 1.5, 100);
-    light.position.set(-2, 50, 20);
-    this.scene.add(light);
-    this.scene.add(ambient);
-
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.controls.update();
-    this.loadModels();
-  }
-
-  loadModels() {
-    const models = {
-      world: { url: "src/res/model/world.gltf" },
-    };
-
-    {
-      const manager = new THREE.LoadingManager();
-      const loader = new GLTFLoader(this.manager);
-      for (const model of Object.values(models)) {
-        loader.load(model.url, (gltf) => {
-          model.gltf = gltf;
-          this.scene.add(gltf.scene);
-          this.train = gltf.scene.children.find(
-            (child) => child.name === "train"
-          );
-          this.mixer = new THREE.AnimationMixer(gltf.scene);
-          gltf.animations.forEach((clip) => {
-            this.mixer.clipAction(clip).play();
-          });
-        });
-      }
-    }
-  }
-
-  render = () => {
-    requestAnimationFrame(this.render);
-    this.update();
-    this.renderer.render(this.scene, this.camera);
-  };
+function render() {
+  requestAnimationFrame(render);
 
   // update camera position and rotation
-  update = () => {
-    var delta = this.clock.getDelta();
-    if (this.mixer) this.mixer.update(delta);
-  };
+  var delta = clock.getDelta();
+  if (mixer) mixer.update(delta / 3);
+
+  camera.position.set(
+    train.position.x,
+    train.position.y + 0.5,
+    train.position.z
+  );
+
+  renderer.render(scene, camera);
 }
 
-const scene = new Scene();
-scene.render();
+// setup
+const clock = new THREE.Clock();
+const scene = new THREE.Scene();
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+camera.position.z = 5;
+camera.position.y = 2;
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
+
+// set background
+scene.background = new THREE.Color("skyblue");
+
+// sun (natural light)
+const ambient = new THREE.AmbientLight("gray");
+const light = new THREE.PointLight("white", 1.5, 100);
+light.position.set(-2, 50, 20);
+scene.add(light);
+scene.add(ambient);
+
+const controls = new PointerLockControls(camera, renderer.domElement);
+
+renderer.domElement.addEventListener("click", function () {
+  controls.lock();
+});
+
+const models = {
+  world: { url: "src/res/model/world.gltf" },
+};
+
+const manager = new THREE.LoadingManager();
+manager.onLoad = init;
+
+function init() {
+  // hide loading bar
+  const loadingBar = document.querySelector("#loading");
+  //loadingBar.style.display = "none";
+}
+
+const loader = new GLTFLoader(manager);
+var train;
+var mixer;
+for (const model of Object.values(models)) {
+  loader.load(model.url, (gltf) => {
+    model.gltf = gltf;
+    scene.add(gltf.scene);
+    train = gltf.scene.children.find((child) => child.name === "train");
+    mixer = new THREE.AnimationMixer(gltf.scene);
+    gltf.animations.forEach((clip) => {
+      mixer.clipAction(clip).play();
+    });
+  });
+}
+
+render();
