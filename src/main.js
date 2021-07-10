cd2ca483864837e955d834b46c91a5b1b24aa7a5
Jennifer Ong <jennifer.ex.ong@gmail.com>
2021-07-10 14:39:11 +1200

    change favicon; add stations to scene model; update scene lighting

--- src/main.js
+++ src/main.js
@@ -2,63 +2,66 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
 
-function render() {
-  requestAnimationFrame(render);
-
-  // update camera position and rotation
-  var delta = clock.getDelta();
-  if (mixer) mixer.update(delta / 3);
-
-  camera.position.set(
-    train.position.x,
-    train.position.y + 0.5,
-    train.position.z
-  );
-
-  renderer.render(scene, camera);
-}
-
 // setup
 const clock = new THREE.Clock();
 const scene = new THREE.Scene();
-const width = window.innerWidth;
-const height = window.innerHeight;
 
-const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+// camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 camera.position.z = 5;
 camera.position.y = 2;
+
+// renderer
 const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
-renderer.setSize(width, height);
+renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 //renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.outputEncoding = THREE.sRGBEncoding;
+
 document.body.appendChild(renderer.domElement);
+
+// window resize
+window.addEventListener("resize", onWindowResize, false);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
+}
 
 // background and fog
 {
   const skyColor = "#77cacf";
-  const near = 20;
+  const near = 15;
   const far = 50;
   scene.fog = new THREE.Fog(skyColor, near, far);
   scene.background = new THREE.Color(skyColor);
 }
 
-// sun (natural light)
-const hemisphere = new THREE.HemisphereLight("white", "black", 0.05);
-const ambient = new THREE.AmbientLight("white", 0.1);
-scene.add(ambient);
-const sun = new THREE.PointLight("#e8dba8", 1);
-sun.position.set(0, 10, 10);
-sun.castShadow = true;
-sun.shadow.bias = -0.0005;
-sun.shadow.mapSize.width = 1024 * 3;
-sun.shadow.radius = 3;
-sun.shadow.mapSize.height = 1024 * 3;
-scene.add(sun);
-scene.add(hemisphere);
+// lighting
+{
+  const hemisphere = new THREE.HemisphereLight("white", "black", 0.4);
+  const ambient = new THREE.AmbientLight("white", 0.1);
+  scene.add(ambient);
+  const sun = new THREE.PointLight("#e8dba8", 1);
+  sun.position.set(0, 20, 10);
+  sun.castShadow = true;
+  sun.shadow.bias = -0.0005;
+  sun.shadow.mapSize.width = 1024 * 3;
+  sun.shadow.radius = 3;
+  sun.shadow.mapSize.height = 1024 * 3;
+  scene.add(sun);
+  scene.add(hemisphere);
+}
 
+// mouse controls
 const controls = new PointerLockControls(camera, renderer.domElement);
 const enterInstructions = document.getElementById("enter");
 const exitInstructions = document.getElementById("exit");
@@ -77,50 +80,51 @@ controls.addEventListener("unlock", () => {
   exitInstructions.style.display = "none";
 });
 
-const models = {
-  world: { url: "src/res/model/world.gltf" },
-};
-
+const world = { url: "src/res/model/world.glb", gltf: undefined };
 const manager = new THREE.LoadingManager();
 manager.onLoad = init;
 
-function init() {}
-
+// load the scene
 const loader = new GLTFLoader(manager);
-var train;
+loader.load(world.url, (gltf) => {
+  world.gltf = gltf;
+  scene.add(gltf.scene);
+});
+
+// initialize shadows, animations
 var mixer;
-for (const model of Object.values(models)) {
-  loader.load(model.url, (gltf) => {
-    model.gltf = gltf;
-    train = gltf.scene.children.find((child) => child.name === "train");
-
-    // ground receives shadows
-    let ground = gltf.scene.children.find((child) => child.name === "ground");
-    ground.receiveShadow = true;
-
-    gltf.scene.traverse((node) => {
-      if (node.isMesh) {
-        node.castShadow = true;
-        node.receiveShadow = true;
-      }
-    });
-
-    scene.add(gltf.scene);
-    // all other objects in the scene cast shadows
-    mixer = new THREE.AnimationMixer(gltf.scene);
-    gltf.animations.forEach((clip) => {
-      mixer.clipAction(clip).play();
-    });
+var train;
+function init() {
+  // cast and receive shadows
+  world.gltf.scene.traverse((node) => {
+    if (node.isMesh) {
+      node.castShadow = true;
+      node.receiveShadow = true;
+    }
   });
-}
 
-// window resize
-window.addEventListener("resize", onWindowResize, false);
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  train = world.gltf.scene.children.find((child) => child.name === "train");
+  mixer = new THREE.AnimationMixer(world.gltf.scene);
+  const clips = world.gltf.animations;
+  const trainClip = clips[0];
+  const trainAction = mixer.clipAction(trainClip);
+  trainAction.play();
+
   render();
 }
 
-render();
+function render() {
+  requestAnimationFrame(render);
+
+  // update camera position and rotation
+  var delta = clock.getDelta();
+  if (mixer) mixer.update(delta / 3);
+
+  camera.position.set(
+    train.position.x,
+    train.position.y + 0.5,
+    train.position.z
+  );
+
+  renderer.render(scene, camera);
+}
